b154f52227b22ecbc0922c8e61baf04976119e67
Mat Lowery <mlowery@pentaho.com>
2009-09-15 17:12:00 +0000

    [CM-88] [CM-102] Added documentation.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-metadata-editor/trunk@39510 5d404267-db13-0410-8476-b1f6b398e63a

--- package-res/set-pentaho-java.bat
+++ package-res/set-pentaho-java.bat
@@ -1,5 +1,29 @@
 rem ---------------------------------------------------------------------------
-rem Find a suitable Java
+rem Finds a suitable Java
+rem
+rem Looks in well-known locations to find a suitable Java then sets two 
+rem environment variables for use in other bat files. The two environment
+rem variables are:
+rem 
+rem * _PENTAHO_JAVA_HOME - absolute path to Java home
+rem * _PENTAHO_JAVA - absolute path to Java launcher (e.g. java.exe)
+rem 
+rem The order of the search is as follows:
+rem 
+rem 1. environment variable PENTAHO_JAVA_HOME - path to Java home
+rem 2. environment variable JAVA_HOME - path to Java home
+rem 3. environment variable JRE_HOME - path to Java home
+rem 4. argument #1 - path to Java home
+rem 
+rem If a suitable Java is found at one of these locations, then 
+rem _PENTAHO_JAVA_HOME is set to that location and _PENTAHO_JAVA is set to the 
+rem absolute path of the Java launcher at that location. If none of these 
+rem locations are suitable, then _PENTAHO_JAVA_HOME is set to empty string and 
+rem _PENTAHO_JAVA is set to java.exe.
+rem 
+rem Finally, there is one final optional environment variable: PENTAHO_JAVA.
+rem If set, this value is used in the construction of _PENTAHO_JAVA. If not 
+rem set, then the value java.exe is used. 
 rem ---------------------------------------------------------------------------
 
 if not "%PENTAHO_JAVA%" == "" goto gotPentahoJava
