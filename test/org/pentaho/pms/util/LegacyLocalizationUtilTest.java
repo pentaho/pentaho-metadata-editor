ef0c0fb7a3627fa553966c41ff4520fc99691261
Curtis Boyden <cboyden@pentaho.com>
2011-04-01 18:32:47 +0000

    [PMD-621] Adding utility to unit test to create test files
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-metadata-editor/trunk@45494 5d404267-db13-0410-8476-b1f6b398e63a

--- test/org/pentaho/pms/util/LegacyLocalizationUtilTest.java
+++ test/org/pentaho/pms/util/LegacyLocalizationUtilTest.java
@@ -1,7 +1,14 @@
 package org.pentaho.pms.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.text.DateFormat;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Properties;
 
 import junit.framework.Assert;
@@ -96,5 +103,58 @@ public class LegacyLocalizationUtilTest {
     
     Assert.assertEquals(275, props.size());
     Assert.assertEquals("Customer", props.get("[LogicalModel-BV_ORDERS].[Category-BC_CUSTOMER_W_TER_].[name]"));
+  }
+  
+  public void buildLargeXmi() throws Exception {
+    XmiParser parser = new XmiParser();
+    Domain domain = parser.parseXmi(new FileInputStream("/temp/metadata.xmi"));
+    
+    LocalizationUtil util = new LocalizationUtil();
+    
+    String locale = "en-US";
+    Properties enUs = util.exportLocalizedProperties(domain, locale);
+    
+    Properties temp = null;
+    
+    Locale[] locales = DateFormat.getAvailableLocales();
+    
+    for(int i = 0; i < locales.length; i++) {
+      temp = new Properties();
+      for(Object key : enUs.keySet()) {
+        temp.put(key, "[" + locales[i].toString() + "] " + enUs.get(key).toString());
+      }
+      util.importLocalizedProperties(domain, temp, locales[i].toString().replace("_","-"));
+    }
+    
+    new FileOutputStream("/temp/multi-locale.xmi").write(parser.generateXmi(domain).getBytes("UTF-8"));
+  }
+  
+  public void buildManyPropBundles() throws Exception {
+    BufferedReader reader = new BufferedReader(new FileReader("/temp/metadata_en_US.properties"));
+    
+    HashMap<String, BufferedWriter> outFiles = new HashMap<String, BufferedWriter>();
+    
+    Locale[] locales = DateFormat.getAvailableLocales();
+    
+    for(int i = 0; i < locales.length; i++) {
+      String locale = locales[i].toString().replace("_","-");
+      outFiles.put(locale, new BufferedWriter(new FileWriter("/temp/props/metadata_" + locale + ".properties")));
+    }
+    
+    String line = null;
+    while((line = reader.readLine()) != null) {
+      for(String locale : outFiles.keySet()) {
+        if(line.endsWith("=")) {
+          outFiles.get(locale).write(line);
+        } else {
+          outFiles.get(locale).write(line.replace("=", "=[" + locale + "]"));
+        }
+        outFiles.get(locale).write("\r\n");
+      }
+    }
+    
+    for(String locale : outFiles.keySet()) {
+      outFiles.get(locale).close();
+    }
   }
 }
