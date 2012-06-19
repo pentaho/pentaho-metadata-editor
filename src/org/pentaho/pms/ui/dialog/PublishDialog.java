3b64af2a39e7bc42e82c8e126eda93fd8d5d596d
Ezequiel Cuellar-Ojeda <ecuellar@pentaho.com>
2012-06-19 18:32:20 +0000

    BISERVER-4473 As a system author, I want to publish a Pentaho Metadata models into the Pentaho Unified Repository (PUR).
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-metadata-editor/trunk@50282 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pms/ui/dialog/PublishDialog.java
+++ src/org/pentaho/pms/ui/dialog/PublishDialog.java
@@ -232,7 +232,7 @@ public class PublishDialog extends TitleAreaDialog {
       File file = new File(DEFAULT_METADATA_FILE);
       file.deleteOnExit();
       InputStream stream = new FileInputStream(file);
-      FormDataMultiPart part = new FormDataMultiPart().field("domainId", domainId + ".xmi", MediaType.TEXT_PLAIN_TYPE).field("metadataFile", stream, MediaType.APPLICATION_XML_TYPE);
+      FormDataMultiPart part = new FormDataMultiPart().field("domainId", domainId, MediaType.TEXT_PLAIN_TYPE).field("metadataFile", stream, MediaType.APPLICATION_XML_TYPE);
       Client client = Client.create();
       client.addFilter(new HTTPBasicAuthFilter(userId, userPassword));
       WebResource resource = client.resource(serverURL);
