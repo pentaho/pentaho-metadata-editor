1927145753d5dbad4697d96c5c15f53e13caf2bb
Ezequiel Cuellar-Ojeda <ecuellar@pentaho.com>
2012-06-19 15:41:41 +0000

    Fix for BISERVER-4473 As a system author, I want to publish a Pentaho Metadata models into the Pentaho Unified Repository (PUR).
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-metadata-editor/trunk@50278 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pms/ui/dialog/PublishDialog.java
+++ src/org/pentaho/pms/ui/dialog/PublishDialog.java
@@ -22,11 +22,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
-import java.util.ResourceBundle;
+
+import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -50,6 +51,11 @@ import org.pentaho.pms.core.CWM;
 import org.pentaho.pms.messages.Messages;
 import org.pentaho.pms.schema.SchemaMeta;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.multipart.FormDataMultiPart;
+
 /**
  * @author wseyler
  *
@@ -57,34 +63,26 @@ import org.pentaho.pms.schema.SchemaMeta;
 public class PublishDialog extends TitleAreaDialog {
   
   private static final String LAST_USED_METADATA_FILE = "metadata.xmi";
-  private static final String LAST_USED_PROP = "last_used"; //$NON-NLS-1$
-  private static final String DEFAULT_SOLUTION = "steel-wheels"; //$NON-NLS-1$
+  private static final String LAST_USED_PROP = "last_used"; //    //publishPassword = tPublishPassword.getText();$NON-NLS-1$
   private static final String URL_PROPS_FILE = "ui/publishUrls.properties"; //$NON-NLS-1$ 
   private static final String METADATA_FILES_FILE ="ui/publishMdFiles.properties"; 
   private static final String SOLUTIONS_PROPS_FILE = "ui/publishSolutions.properties";
-  private static final String DEFAULT_METADATA_FILE= "metadata.xmi"; 
-  private static final String DEFAULT_PUBLISH_URL = "http://localhost:8080/pentaho/RepositoryFilePublisher"; //$NON-NLS-1$
-  private static final String DEFAULT_FILENAME = "metadata.xmi";
+  private static final String DEFAULT_PUBLISH_URL = "http://localhost:8080/pentaho/plugin/data-access/api/metadata/import"; //$NON-NLS-1$
+  private static final String DEFAULT_METADATA_FILE= "metadata.xmi";
   private SchemaMeta schemaMeta;
   
   private LogWriter log;
   private PropsUI props;
   
   private String serverURL;
-  private String solutionName;
-  private String fileName; //$NON-NLS-1$
-  
   private String userId;
   private String userPassword;
-  private String publishPassword;
   
   private Combo tServerURL;
-  private Combo tSolutionName;
   private Combo tMdFileName;
   
   private Text tUserId;
   private Text tUserPassword;
-  private Text tPublishPassword;
   
   private Properties publishUrls;
   private Properties solutionFolders;
@@ -122,40 +120,9 @@ public class PublishDialog extends TitleAreaDialog {
     data.grabExcessHorizontalSpace = true;
     data.minimumWidth = 470;
 
-    Label fnLabel = new Label (c1, SWT.NONE);
-    fnLabel.setText (Messages.getString("PublishDialog.LABEL_FILENAME"));
-    //fnLabel.setText("Solution File Name");
     data = new GridData();
     data.grabExcessHorizontalSpace = true;
     data.minimumWidth = 470;
-    fnLabel.setLayoutData (data);
-
-    c0.setBackground(fnLabel.getBackground());
-    c1.setBackground(fnLabel.getBackground());
-
-    tMdFileName = new Combo (c1, SWT.BORDER);
-    data = new GridData();
-    data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 470;
-    tMdFileName.setText(DEFAULT_FILENAME); 
-    tMdFileName.setLayoutData (data);
-
-    Label label0 = new Label (c1, SWT.NONE);
-    label0.setText (Messages.getString("PublishDialog.LABEL_SOLUTION"));
-    data = new GridData();
-    data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 470;
-    label0.setLayoutData (data);
-    
-/*    c0.setBackground(label0.getBackground());
-    c1.setBackground(label0.getBackground());*/
-
-    tSolutionName = new Combo (c1, SWT.BORDER);
-    data = new GridData();
-    data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 470;
-    tSolutionName.setText(DEFAULT_SOLUTION); 
-    tSolutionName.setLayoutData (data);
 
     Label label2 = new Label (c1, SWT.NONE);
     label2.setText (Messages.getString("PublishDialog.LABEL_SERVER"));
@@ -163,48 +130,25 @@ public class PublishDialog extends TitleAreaDialog {
     data.grabExcessHorizontalSpace = true;
     data.minimumWidth = 470;
     label2.setLayoutData (data);
-
+    
+    c0.setBackground(label2.getBackground());
+    c1.setBackground(label2.getBackground());
+    
+    
     tServerURL = new Combo(c1, SWT.DROP_DOWN);
     data = new GridData();
     data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 470;
+    data.minimumWidth = 530;
     tServerURL.setLayoutData (data);
     
     populateServerUrl();
     populateSolutionFolders();
     populateMetadataFiles();
     
-    Label label4 = new Label (c1, SWT.NONE);
-    label4.setText (Messages.getString("PublishDialog.LABEL_PUBLISH_PASSWORD"));
-    data = new GridData();
-    data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 300;
-    label4.setLayoutData (data);
-
     data = new GridData();
     data.grabExcessHorizontalSpace = true;
     data.minimumWidth = 300;
     
-    tPublishPassword = new Text (c1, SWT.BORDER | SWT.PASSWORD);
-    data = new GridData();
-    data.grabExcessHorizontalSpace = true;
-    data.minimumWidth = 300;
-    tPublishPassword.setLayoutData (data);
-    // Add code to check for a properties file containing the default
-    // publish password. For this to work, the file needs to be located in the
-    // lib directory if it is to be found.
-    try {
-      ResourceBundle bundle = ResourceBundle.getBundle("publishpassword"); //$NON-NLS-1$
-      String defaultPassword = bundle.getString("default.password"); //$NON-NLS-1$
-      if ( (defaultPassword != null) && (defaultPassword.length() > 0) ) {
-        // System.out.println("Default Password:" + defaultPassword);
-        tPublishPassword.setText(defaultPassword);
-      }
-    } catch (Exception ex) {
-      // No publishpassword.properties
-      // no need to log this, it's not an error if this occurs.
-    }
-
     Label label6 = new Label (c1, SWT.NONE);
     label6.setText (Messages.getString("PublishDialog.LABEL_USER"));
     data = new GridData();
@@ -279,40 +223,35 @@ public class PublishDialog extends TitleAreaDialog {
     if (!populateStrings()) {
       return false;
     }
-    
-    CWM cwmInstance = CWM.getInstance(schemaMeta.getDomainName());
+    String domainId = schemaMeta.getDomainName();
+    CWM cwmInstance = CWM.getInstance(domainId);
     try {
       String xmi = cwmInstance.getXMI();
-      BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+      BufferedWriter out = new BufferedWriter(new FileWriter(DEFAULT_METADATA_FILE));
       out.write(xmi);
       out.close();
-      File file = new File(fileName);
+      File file = new File(DEFAULT_METADATA_FILE);
       file.deleteOnExit();
-      File[] files = {file};
-      int result = PublisherUtil.publish(serverURL, solutionName, files, publishPassword, userId, userPassword, false);
-      if (result == PublisherUtil.FILE_EXISTS) {
-        MessageBox mb = new MessageBox(getShell(), SWT.NO | SWT.YES | SWT.ICON_WARNING);
-        mb.setText(Messages.getString("PublishDialog.FILE_EXISTS")); //$NON-NLS-1$
-        mb.setMessage(Messages.getString("PublishDialog.FILE_OVERWRITE")); //$NON-NLS-1$
-        if (mb.open() == SWT.YES) {
-          result = PublisherUtil.publish(serverURL, solutionName, files, publishPassword, userId, userPassword, true);
-        } else {
+      InputStream stream = new FileInputStream(file);
+      FormDataMultiPart part = new FormDataMultiPart().field("domainId", domainId, MediaType.TEXT_PLAIN_TYPE).field("metadataFile", stream, MediaType.APPLICATION_XML_TYPE);
+      Client client = Client.create();
+      client.addFilter(new HTTPBasicAuthFilter(userId, userPassword));
+      WebResource resource = client.resource(serverURL);
+      String result = resource.type(MediaType.MULTIPART_FORM_DATA_TYPE).put(String.class, part);
+      if(result.equals("SUCCESS")) {
+    	  MessageBox mb = new MessageBox(getShell(), SWT.OK | SWT.ICON_INFORMATION);
+          mb.setText(Messages.getString("PublishDialog.ACTION_SUCCEEDED")); //$NON-NLS-1$
+          mb.setMessage(Messages.getString("PublishDialog.FILE_SAVE_SUCCEEDED", domainId)); //$NON-NLS-1$
+          mb.open();
+          dispose();
+          return true;
+      } else {
+    	  MessageBox mb = new MessageBox(getShell(), SWT.OK | SWT.ICON_ERROR);
+          mb.setText(Messages.getString("PublishDialog.ACTION_FAILED")); //$NON-NLS-1$
+          mb.setMessage(Messages.getString("PublishDialog.FILE_SAVE_FAILED", domainId)); //$NON-NLS-1$
+          mb.open();
           return false;
-        }
-      }
-      if (result != PublisherUtil.FILE_ADD_SUCCESSFUL) {
-        MessageBox mb = new MessageBox(getShell(), SWT.OK | SWT.ICON_ERROR);
-        mb.setText(Messages.getString("PublishDialog.ACTION_FAILED")); //$NON-NLS-1$
-        mb.setMessage(Messages.getString("PublishDialog.FILE_SAVE_FAILED", fileName)); //$NON-NLS-1$
-        mb.open();
-        return false;
-      } else {  // We did it!
-        MessageBox mb = new MessageBox(getShell(), SWT.OK | SWT.ICON_INFORMATION);
-        mb.setText(Messages.getString("PublishDialog.ACTION_SUCCEEDED")); //$NON-NLS-1$
-        mb.setMessage(Messages.getString("PublishDialog.FILE_SAVE_SUCCEEDED", fileName)); //$NON-NLS-1$
-        mb.open();
-        dispose();
-        return true;
+    	  
       }
     } catch (Exception e) {
       new ErrorDialog(
@@ -320,11 +259,7 @@ public class PublishDialog extends TitleAreaDialog {
           Messages.getString("General.USER_TITLE_ERROR"), Messages.getString("PublishDialog.ACTION_FAILED"), e); //$NON-NLS-1$ //$NON-NLS-2$
       return false;
     } finally {
-    
-      // update the props file even if the connection fails
-      updateSolutionsPropsFile();
       updateUrlPropsFile();
-      updateMetadataFilesPropsFile();
     }
   }
   
@@ -370,94 +305,6 @@ public class PublishDialog extends TitleAreaDialog {
       }
     }
   }
-
-  private void updateSolutionsPropsFile() {
-	    // update solution folders file
-	    boolean lastUsedChanged = false;
-	    if ((solutionFolders.getProperty(LAST_USED_PROP) == null) || 
-	        !solutionFolders.getProperty(LAST_USED_PROP).equals(solutionName)) {
-	      lastUsedChanged = true;
-	    }
-	    solutionFolders.setProperty(LAST_USED_PROP, solutionName);
-	    boolean newSolution = true;
-	    for (Object pname : solutionFolders.keySet()) {
-	      String paramName = pname.toString();
-	      if (!paramName.equals(LAST_USED_PROP)) {
-	        if (solutionFolders.getProperty(paramName).equals(solutionName)) {
-	          newSolution = false;
-	        }
-	      }
-	    }
-	    if (newSolution) {
-	      solutionFolders.setProperty("file" + solutionFolders.size(), solutionName); //$NON-NLS-1$
-	    }
-	    
-	    if (newSolution || lastUsedChanged) {
-	      FileOutputStream fos = null;
-	      try {
-	        fos = new FileOutputStream(SOLUTIONS_PROPS_FILE);
-	        solutionFolders.store(fos, "Pentaho Metadata Solution Folders."); //$NON-NLS-1$
-	      } catch (IOException e) {
-	        new ErrorDialog(
-	            getShell(),
-	            Messages.getString("General.USER_TITLE_ERROR"), Messages.getString("PublishDialog.ACTION_FAILED"), e); //$NON-NLS-1$ //$NON-NLS-2$
-
-	      } finally {
-	        try {
-	          if (fos != null) {
-	            fos.close();
-	          }
-	        } catch (Exception e) {
-	          // ignore any close exceptions
-	        }
-	      }
-	    }
-	  }
-  
-  
-  private void updateMetadataFilesPropsFile() {
-	    // update metadata files file
-	    boolean lastUsedChanged = false;
-	    if ((mdFiles.getProperty(LAST_USED_METADATA_FILE) == null) || 
-	        !mdFiles.getProperty(LAST_USED_METADATA_FILE).equals(fileName)) {
-	      lastUsedChanged = true;
-	    }
-	    mdFiles.setProperty(LAST_USED_METADATA_FILE, fileName);
-	    boolean newFile = true;
-	    for (Object pname : mdFiles.keySet()) {
-	      String paramName = pname.toString();
-	      if (!paramName.equals(LAST_USED_METADATA_FILE)) {
-	        if (mdFiles.getProperty(paramName).equals(fileName)) {
-	          newFile = false;
-	        }
-	      }
-	    }
-	    if (newFile) {
-	      mdFiles.setProperty("file" + mdFiles.size(), fileName); //$NON-NLS-1$
-	    }
-	    
-	    if (newFile || lastUsedChanged) {
-	      FileOutputStream fos = null;
-	      try {
-	        fos = new FileOutputStream(METADATA_FILES_FILE);
-	        mdFiles.store(fos, "Pentaho Metadata Files."); //$NON-NLS-1$
-	      } catch (IOException e) {
-	        new ErrorDialog(
-	            getShell(),
-	            Messages.getString("General.USER_TITLE_ERROR"), Messages.getString("PublishDialog.ACTION_FAILED"), e); //$NON-NLS-1$ //$NON-NLS-2$
-
-	      } finally {
-	        try {
-	          if (fos != null) {
-	            fos.close();
-	          }
-	        } catch (Exception e) {
-	          // ignore any close exceptions
-	        }
-	      }
-	    }
-	  }
-  
   
   private void populateServerUrl() {
     String lastUsedUrl = ""; //$NON-NLS-1$
@@ -514,8 +361,7 @@ public class PublishDialog extends TitleAreaDialog {
 	        fis = new FileInputStream(file);
 	        solutionFolders.load(fis);
 	      } catch (IOException ex) {
-	        // populate the dialog with a default value
-	        tSolutionName.setText(DEFAULT_SOLUTION);
+	    	// ignore any exceptions
 	      } finally {
 	        if (fis != null) {
 	          try {
@@ -535,18 +381,12 @@ public class PublishDialog extends TitleAreaDialog {
 	            folders.add(solutionFolders.getProperty(paramName));
 	          }
 	        }
-	        tSolutionName.setItems(folders.toArray(new String[0]));
 	        // set the default value if available
 	        if (StringUtils.isBlank(lastUsedSolution) && folders.size() > 0) {
 	          lastUsedSolution = folders.get(0);
 	        }
-	        tSolutionName.setText(lastUsedSolution);
-	      } else {
-	    	  tSolutionName.setText(DEFAULT_SOLUTION);
 	      }
-	    } else {
-	    	tSolutionName.setText(DEFAULT_SOLUTION);
-	    }
+	    } 
 	  }
 	  
   private void populateMetadataFiles() {
@@ -559,8 +399,7 @@ public class PublishDialog extends TitleAreaDialog {
 	        fis = new FileInputStream(file);
 	        mdFiles.load(fis);
 	      } catch (IOException ex) {
-	        // populate the dialog with a default value
-	        tMdFileName.setText(DEFAULT_METADATA_FILE);
+	    	    // ignore any exceptions
 	      } finally {
 	        if (fis != null) {
 	          try {
@@ -586,12 +425,8 @@ public class PublishDialog extends TitleAreaDialog {
 	          lastUsedMdFile = files.get(0);
 	        }
 	        tMdFileName.setText(lastUsedMdFile);
-	      } else {
-	    	  tMdFileName.setText(DEFAULT_METADATA_FILE);
-	      }
-	    } else {
-	    	tMdFileName.setText(DEFAULT_METADATA_FILE);
-	    }
+	      } 
+	    } 
 	  }
 
   
@@ -599,27 +434,10 @@ public class PublishDialog extends TitleAreaDialog {
    * 
    */
   private boolean populateStrings() {
-    String seperatorFwd = "/"; //$NON-NLS-1$
-    String seperatorBck = "\\"; //$NON-NLS-1$
-    
     serverURL = tServerURL.getText();
-//    if (!serverURL.endsWith(seperatorFwd)) {
-//      serverURL += seperatorFwd;
-//    }
-    solutionName = tSolutionName.getText();
-    fileName = tMdFileName.getText();
-    if (solutionName.indexOf(seperatorFwd) >= 0 || solutionName.indexOf(seperatorBck) >= 0) {
-      MessageBox mb = new MessageBox(getShell(), SWT.OK | SWT.ICON_ERROR);
-      mb.setText(Messages.getString("PublishDialog.LOCATION_ERROR")); //$NON-NLS-1$
-      mb.setMessage(Messages.getString("PublishDialog.LOCATION_ERROR_INFO")); //$NON-NLS-1$
-      mb.open();
-      return false;
-    }
-   
     userId = tUserId.getText();
     userPassword = tUserPassword.getText();
-    publishPassword = tPublishPassword.getText();
-    return true;
+    return !StringUtils.isEmpty(serverURL) && !StringUtils.isEmpty(userId) && !StringUtils.isEmpty(userPassword);
   }
 
   private void cancel() {
