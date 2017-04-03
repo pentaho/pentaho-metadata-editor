a371b59d31f7f73f4904276246f82bbceb9d59e1
ecuellar <ecuellar@R90GVDJU.pentaho.com>
2017-04-03 16:01:50 -0400

    [BACKLOG-15645] PME - Save Domain

--- test/org/pentaho/pms/ui/MetaEditorTest.java
+++ test/org/pentaho/pms/ui/MetaEditorTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.pms.ui;
@@ -161,7 +161,7 @@ public class MetaEditorTest {
     } ).when( me ).refreshAll();
     me.setSchemaMeta( mock( SchemaMeta.class ) );
     me.newFile();
-    assertEquals( me.getSchemaMeta().domainName, "NewDomain1" );
+    assertEquals( me.getSchemaMeta().domainName, "" );
   }
 
   private Map<String, String[]> mockForGetTables( String prefferedSchemaName ) throws KettleDatabaseException {
