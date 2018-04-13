6ffe974f086e23a791f906ae4e94ba1a6447351a
Vitor Cardoso <vitor.cardoso@pentaho.com>
2018-04-13 14:25:31 +0100

    [BACKLOG-22796] - 8.1 - Update Copyright Script have 2017 instead of 2018

--- assemblies/pme-ce/src/main/resources/metadata-editor.sh
+++ assemblies/pme-ce/src/main/resources/metadata-editor.sh
@@ -13,7 +13,7 @@
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
 #
-# Copyright (c) 2009-2017 Hitachi Vantara..  All rights reserved.
+# Copyright (c) 2009-2018 Hitachi Vantara..  All rights reserved.
 
 # this script must be executed inside the pme folder
 
