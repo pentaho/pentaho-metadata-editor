487a9656f85e2f4429f1c923891d8d3d0400de6a
Silvia Fernandes <silvia.fernandes1@hitachivantara.com>
2026-03-26 22:31:00 +0000

    chore: address copilot review comments

--- editor/src/main/java/org/pentaho/pms/ui/QueryBuilderDialog.java
+++ editor/src/main/java/org/pentaho/pms/ui/QueryBuilderDialog.java
@@ -564,14 +564,14 @@ public class QueryBuilderDialog extends Dialog {
   }
 
   @Override
-  public void okPressed() {
+  protected void okPressed() {
     // Ensure that if the getMqlQuery operation fails, an error message shows and the dialog is not closed
     try {
       getMqlQuery();
       super.okPressed();
     } catch ( Exception e ) {
-      new ErrorDialog( getParentShell(), Messages.getString( "General.USER_TITLE_ERROR" ),
-        Messages.getString( "QueryDialog.USER_ERROR_LOADING_QUERY" ), e );
+      new ErrorDialog( getShell(), Messages.getString( "General.USER_TITLE_ERROR" ),
+        Messages.getString( "QueryDialog.USER_ERROR_GENERATING_QUERY" ), e );
     }
   }
 }
